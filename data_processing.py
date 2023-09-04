41442ec7431ee96d8e370139671bda6b34f18b41
ymarcais <119794979+ymarcais@users.noreply.github.com>
2023-09-04 13:26:54 +0200

    data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -8,7 +8,6 @@ lib_py_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'lib
 sys.path.append(lib_py_path)
 from stats import Statistician
 
-
 @dataclass
 class Data_processing:
 	st: Statistician
